da4c36e47372b257e6f5074b09087f7067ff81fc
KVRTeJ <bb.sssss@inbox.ru>
2024-03-04 01:16:01 +0700

    Создан проект

---
+++ Binary_Tree/Binary_Tree/Binary_Tree.hpp
@@ -0,0 +1,6 @@
+#ifndef Binary_Tree_hpp
+#define Binary_Tree_hpp
+
+#include <iostream>
+
+#endif /* Binary_Tree_hpp */
---
+++ Binary_Tree/Binary_Tree/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Binary_Tree.hpp"
+
+int main() {
+
+    return 0;
+}
