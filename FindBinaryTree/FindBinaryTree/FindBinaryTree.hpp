2119901d3933504f5b529df3ae8c0c804bc8100a
KVRTeJ <bb.sssss@inbox.ru>
2024-03-19 00:27:25 +0700

    Revert "Revert "Создан проект""
    This reverts commit cea274207f23018b0044f97425d04f6ffd88e2ef.

---
+++ FindBinaryTree/FindBinaryTree/FindBinaryTree.hpp
@@ -0,0 +1,18 @@
+#ifndef FindBinaryTree_hpp
+#define FindBinaryTree_hpp
+
+#include <iostream>
+
+#include "Binary_Tree.hpp"
+
+class FindBinaryTree : public BinaryTree {
+public:
+    FindBinaryTree() :
+    BinaryTree()
+    {}
+    
+private:
+    
+};
+
+#endif /* FindBinaryTree_hpp */
