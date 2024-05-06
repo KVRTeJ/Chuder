fed82527b65d774aae778cf0bfccedc03934cf53
KVRTeJ <bb.sssss@inbox.ru>
2024-05-06 21:46:22 +0700

    Создан проект

---
+++ Huffman_Coding/Huffman_Coding/HuffmanTree.hpp
@@ -0,0 +1,28 @@
+#ifndef HuffmanTree_hpp
+#define HuffmanTree_hpp
+
+#include <iostream>
+
+class HuffmanTree {
+public:
+    class Node;
+public:
+    HuffmanTree() = default;
+    ~HuffmanTree() = default;
+    
+private:
+    Node* m_root = nullptr;
+};
+
+class HuffmanTree::Node {
+public:
+    explicit Node(std::string symbols) = default;
+    ~Node() = default;
+    
+private:
+    std::string m_data = {};
+    Node* m_left = nullptr;
+    Node* m_right = nullptr;
+};
+
+#endif /* HuffmanTree_hpp */
